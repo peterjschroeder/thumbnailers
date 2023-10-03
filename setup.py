9824cf1ea1957d748db6562bee328149fbd10ef3
peter <peterjschroeder@gmail.com>
2023-10-03 12:10:51 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+        name='thumbnailers', 
+        version='0.1', 
+        description='Thumbnailers', 
+        author='Peter J. Schroeder', 
+        author_email='peterjschroeder@gmail.com', 
+        url='https://github.com/peterjschroeder/thumbnailers',
+        scripts=['cooklang-thumbnailer'],
+        install_requires=[]
+)
+
